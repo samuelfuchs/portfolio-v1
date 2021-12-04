12911f301955baec70d1b933bd3e5aac7d62ad8b
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-04 19:02:45 -0300

    header offset

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,6 +150,13 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive)
 
 // CHANGE BACKGROUND HEADER
+function scrollHeader() {
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add('scroll-header')
+  else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 // SHOW SCROLL TOP
 
