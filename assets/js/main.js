fc62ed1310b9f00f3377f134da7408e9c4374e6d
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-04 19:12:04 -0300

    scroll to top btn done

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,6 +158,13 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 
-// SHOW SCROLL TOP
+// SHOW SCROLL UP
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll')
+  else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 // DARK LIGHT THEME
