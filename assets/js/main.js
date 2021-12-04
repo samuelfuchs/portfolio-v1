4cb82df97721fb015029647230e25c1574c408e7
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-04 03:18:05 -0300

    qualification section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,5 +50,23 @@ skillsHeader.forEach((el) => {
 })
 
 // QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContent.forEach((tabContent) => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
 
 // SERVICES MODAL
